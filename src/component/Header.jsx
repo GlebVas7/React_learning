f9edc3a4da94f566f159ca628ec830c2fd5cbc6e
Gleb79654588110 <Gleb79654588110@yandex.ru>
2024-01-16 14:16:12 +0300

    first commit

---
+++ src/component/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://styleall.ru/wp-content/uploads/2023/02/4eebee67599783.y3jvccwymzyxlde4ndcsntcyldmxnq.jpg"
+                 alt="" className="image"/>
+        </header>
+    );
+}
+export default Header;
---
+++ src/component/Profile.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <img className="image_content"
+                 src="https://ts11.tarafdari.com/contents/user787898/content-image/lake-landscape-mountain-mountains.jpeg"
+                 alt=""/>
+            <div>
+                ava
+            </div>
+            <div>
+                post
+                <div>
+                    new post
+                </div>
+            </div>
+            <div>
+                send post
+            </div>
+        </div>
+    );
+}
+export default Profile;
